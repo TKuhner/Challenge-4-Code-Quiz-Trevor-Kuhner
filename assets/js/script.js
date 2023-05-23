d12adb8262f5739e67f1fb15547e61b0b8ba580e
TKuhner <trevorkuhner29@gmail.com>
2023-05-23 07:45:18 -0500

    quiz finished. Moving to displaying results and receiving initials. Next is high score handling. Then finally displaying sections as they are active.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,11 @@ const quizData = [
     {
         question: "What is FIFO?",
 
-        a:     "First in Few Out",
-        b:     "Few In Few Out",
-        c:     "First In First Out",
-        d:     "Few In First Out",
-        correct: "First In First Out", 
+        a: "First in Few Out",
+        b: "Few In Few Out",
+        c: "First In First Out",
+        d: "Few In First Out",
+        correct: "First In First Out",
     },
     {
         question: "Which is NOT a datatype?",
@@ -23,8 +23,8 @@ const quizData = [
         b: "string",
         c: "double",
         d: "letter",
-        correct: "letter", 
-    }, 
+        correct: "letter",
+    },
     {
         question: "What year did JavaScript Launch?",
         a: "1995",
@@ -35,29 +35,38 @@ const quizData = [
     }
 ]
 
+var timeInt
 
-var timerEl = document.querySelector("#timer")
-var startButton = document.querySelector("#start")
+// sections
+var introEl = document.querySelector('#intro');
+var quizEl = document.querySelector('#quiz');
+var resultsEl = document.querySelector('#results');
+var scoresEl = document.querySelector('#scores');
 
+// buttons + timer
+var timerEl = document.querySelector("#timer");
+var startButton = document.querySelector("#start");
+
+// counters
+var answersCorrect = 0;
 var secondsCount = 100;
-var currentQuest = 0
+var currentQuest = 0;
 
-const questionEl = document.querySelector("#question")
+const questionEl = document.querySelector("#question");
 // Answer buttons
 const ans1 = document.querySelector("#answer1");
 const ans2 = document.querySelector("#answer2");
 const ans3 = document.querySelector("#answer3");
 const ans4 = document.querySelector("#answer4");
-const answerButton = document.querySelector("#answers")
-
+const answerButton = document.querySelector("#answers");
 
 // EVENT LISTENERS
 startButton.addEventListener("click", startQuiz);
-
 answerButton.addEventListener("click", checkAnswer);
 
+// 
 
-function startQuiz(){
+function startQuiz() {
     // display intro off
     // display quiz on
     startTimer()
@@ -75,37 +84,60 @@ function setQuiz() {
     ans4.textContent = currentQuestion.d
 }
 
-function checkAnswer(event){
+function checkAnswer(event) {
     // alert("made it to checkAnswer")
     event.preventDefault();
 
-    if(event.target.matches(".answer")){
-        console.log(event.target.textContent)
-        if (quizData[currentQuest].correct !== event.target.textContent){
-            secondsCount -= 10;
-            if (currentQuest < quizData.length){
-                currentQuest++;
-                console.log(currentQuest)
-            }
-        } else if (currentQuest < quizData.length){
+
+    console.log(event.target.textContent)
+    if (quizData[currentQuest].correct !== event.target.textContent) {
+        secondsCount -= 10;
+        if (currentQuest < quizData.length) {
             currentQuest++;
-            console.log(currentQuest)
+            if (currentQuest < quizData.length) {
+                setQuiz(currentQuest)
+            }
+            else {
+                getScores();
+            }
+        }else{
+            getScores();
         }
-        setQuiz(currentQuest);
-    }
+
+    } else if (currentQuest < quizData.length) {
+        currentQuest++;
+        answersCorrect++;
+        if (currentQuest < quizData.length) {
+            setQuiz(currentQuest);
+        }
+        else {
+            getScores();
+        }
     }
 
-    
+
+}
+
+function getScores() {
+    clearInterval(timeInt)
+    console.log("Time Remaining " + secondsCount)
+    console.log("Current quest " + currentQuest)
+    console.log("Answers correct " + answersCorrect)
+}
+
+
 
 // Timer function
-function startTimer(){
-    var timeInt = setInterval(function() {
+function startTimer() {
+    timeInt = setInterval(function () {
         secondsCount--;
         timerEl.textContent = 'Time: ' + secondsCount + 's';
 
-        if (secondsCount === 0){
+        if (secondsCount === 0) {
             clearInterval(timeInt);
 
         }
     }, 1000);
 }
+
+
