fff453a3d5ea082323ea72bbbd022f12a326805e
TKuhner <trevorkuhner29@gmail.com>
2023-05-23 19:37:25 -0500

    finished functionality and testing. Last Intro button is not working, but all other functionalities are.

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,15 +42,25 @@ var introEl = document.querySelector('#intro');
 var quizEl = document.querySelector('#quiz');
 var resultsEl = document.querySelector('#results');
 var scoresEl = document.querySelector('#scores');
+var scoresListEl = document.querySelector('#scoresList');
+var initialsInputEl = document.querySelector('#initialsInput')
 
 // buttons + timer
 var timerEl = document.querySelector("#timer");
 var startButton = document.querySelector("#start");
+var submitButton = document.querySelector(".submitButton");
+var scoresButtons = document.querySelectorAll(".scoresButton");
+var clearScoresButton = document.querySelector("#clearScoresButton");
+var returnIntroButton = document.querySelector(".returnIntroButton");
 
 // counters
 var answersCorrect = 0;
 var secondsCount = 100;
 var currentQuest = 0;
+var scoreList = [];
+var userInitials = "";
+var userScore = 0;
+
 
 const questionEl = document.querySelector("#question");
 // Answer buttons
@@ -63,25 +73,57 @@ const answerButton = document.querySelector("#answers");
 // EVENT LISTENERS
 startButton.addEventListener("click", startQuiz);
 answerButton.addEventListener("click", checkAnswer);
+submitButton.addEventListener("click", submitData);
+scoresEl.addEventListener("click", displayScores)
+scoresButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+        displayScores(event.target);
+    });
+});
+clearScoresButton.addEventListener("click", clearScores);
+returnIntroButton.addEventListener("click", returnIntro);
+document.querySelectorAll(".returnIntroButton").forEach(function (button) {
+    button.addEventListener("click", returnIntro);
+  });
 
-// 
+function returnIntro() {
+    // Hide other sections
+    quizEl.style.display = "none";
+    resultsEl.style.display = "none";
+    scoresEl.style.display = "none";
+
+    // Display intro section
+    introEl.style.display = "block";
+}
+
 
 function startQuiz() {
-    // display intro off
-    // display quiz on
-    startTimer()
-    setQuiz(currentQuest);
+    // Reset variables
+    answersCorrect = 0;
+    secondsCount = 100;
+    currentQuest = 0;
+
+    // Display intro off
+    // Display intro section
+    introEl.style.display = "none";
+
+    // Display quiz section
+    quizEl.style.display = "block";
+
+    // Display quiz on
+    startTimer();
+    setQuiz();
 }
 
 function setQuiz() {
-
-    var currentQuestion = quizData[currentQuest]
-
-    questionEl.textContent = currentQuestion.question
-    ans1.textContent = currentQuestion.a
-    ans2.textContent = currentQuestion.b
-    ans3.textContent = currentQuestion.c
-    ans4.textContent = currentQuestion.d
+    if (currentQuest < quizData.length) {
+        var currentQuestion = quizData[currentQuest];
+        questionEl.textContent = currentQuestion.question;
+        ans1.textContent = currentQuestion.a;
+        ans2.textContent = currentQuestion.b;
+        ans3.textContent = currentQuestion.c;
+        ans4.textContent = currentQuestion.d;
+    }
 }
 
 function checkAnswer(event) {
@@ -100,7 +142,7 @@ function checkAnswer(event) {
             else {
                 getScores();
             }
-        }else{
+        } else {
             getScores();
         }
 
@@ -120,12 +162,82 @@ function checkAnswer(event) {
 
 function getScores() {
     clearInterval(timeInt)
-    console.log("Time Remaining " + secondsCount)
-    console.log("Current quest " + currentQuest)
-    console.log("Answers correct " + answersCorrect)
+
+    // Hide quiz section
+    quizEl.style.display = "none";
+
+    // Display results section
+    resultsEl.style.display = "block";
+
+    document.getElementById("scoreReveal").innerText = "Your final score is " + answersCorrect + "/" + currentQuest + " with a remaining time of " + secondsCount + "s";
+
 }
 
+function submitData() {
+    userInitials = initialsInputEl.value;
+    userScore = currentQuest !== 0 ? ((answersCorrect / currentQuest) * 100).toFixed(2) : 0;
+    scoreList.push({ initials: userInitials, score: userScore, time: secondsCount });
 
+    scoreList.sort(function (a, b) {
+        return b.score - a.score;
+    });
+
+    scoresListEl.innerHTML = ""; // Clear existing list items before adding new ones
+
+    scoreList.forEach(function (item) {
+        var listItem = document.createElement("li");
+        listItem.textContent = item.initials + " - Score: " + item.score + " - Time: " + item.time + "s";
+        scoresListEl.appendChild(listItem);
+    });
+
+    var scoresSection = document.getElementById("scores");
+    scoresSection.insertBefore(scoresListEl, scoresSection.getElementsByTagName("h2")[0].nextSibling);
+
+    storeScores(); // Call the storeScores function to save the scores to local storage
+    displayScores();
+}
+
+function displayScores() {
+    introEl.style.display = "none";
+    quizEl.style.display = "none";
+    resultsEl.style.display = "none";
+
+    // Display scores section
+    scoresEl.style.display = "block";
+
+    var storedScores = JSON.parse(localStorage.getItem("scoreList"));
+
+    // Clear existing list items
+    scoresListEl.innerHTML = "";
+
+    // If there are stored scores, display them
+    if (storedScores) {
+        // Sort scores in descending order
+        storedScores.sort(function (a, b) {
+            return b.score - a.score;
+        });
+
+        // Iterate over the scores and create list items
+        storedScores.forEach(function (score) {
+            var listItem = document.createElement("li");
+            listItem.textContent = score.initials + " - Score: " + score.score + " - Time: " + score.time + "s";
+            scoresListEl.appendChild(listItem);
+        });
+    }
+
+    var returnIntroButton = document.querySelector(".returnIntroButton");
+    returnIntroButton.addEventListener("click", returnIntro);
+}
+
+function storeScores() {
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
+    return;
+}
+
+function clearScores() {
+    localStorage.clear();
+    scoresListEl.innerHTML = "";
+}
 
 // Timer function
 function startTimer() {
